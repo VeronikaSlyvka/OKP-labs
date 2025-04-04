f3a94a7e3288fcf462bdba0d57ab0416b197a905
VeronikaSlyvka <veronikaslyvka@gmail.com>
2025-04-05 02:56:38 +0300

    lab5 task1

---
+++ script5.js
@@ -0,0 +1,20 @@
+function dialog(){
+    let total=0;
+    let answer;
+    do{
+        let bookAmount;
+
+        do{
+            bookNumber = +prompt("Введіть артикул книги, яку ви хочете придбати", "1");        
+            if(isNaN(bookNumber) || bookNumber < 0){
+                alert("Будь ласка, введіть число більше 0.");
+            }
+        } while (isNaN(bookNumber) || bookNumber < 0);   
+
+        total += 1;
+        alert(`${total} книг додано до кошика.`);
+        answer = confirm("Бажаєте додати ще одну книгу до кошика?");
+    } while(answer)
+
+
+}
